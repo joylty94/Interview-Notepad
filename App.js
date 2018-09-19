23c97896c49c6538e9f6639cfd418abee3adea5f
joylty94 <wowmen1757@gmail.com>
2018-09-20 02:58:21 +0900

    react-navigation 추가

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import LoginScreenContainer from "./containers/LoginScreenContainer";
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux"
+import { createStackNavigator } from "react-navigation"
 
 import store from "./store"
+import InitialScreenContainer from "./containers/InitialScreenContainer";
+import LoginScreenContainer from "./containers/LoginScreenContainer";
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <LoginScreenContainer/>
+        <StackApp/>
       </Provider>
     );
   }
 }
+
+const StackApp = createStackNavigator({
+  InitialScreen: {
+    screen: InitialScreenContainer,
+  },
+  LoginScreen: {
+    screen: LoginScreenContainer
+  }
+})
