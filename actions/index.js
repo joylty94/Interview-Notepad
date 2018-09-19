5b1ef52a9702bda979a0723b666baf953b68a7c6
joylty94 <wowmen1757@gmail.com>
2018-09-19 23:44:27 +0900

    LoginScreen action 생성

---
+++ actions/index.js
@@ -0,0 +1,9 @@
+import { JOIN_PAGINATION, LOGIN_PAGINATION } from "./actionTypes";
+
+export const Joinpagination = () => ({
+  type: JOIN_PAGINATION,
+});
+
+export const loginpagination = () => ({
+  type: LOGIN_PAGINATION,
+})
