3ad915e43ae3078a2efe16f8c3a3112d4595968e
joylty94 <wowmen1757@gmail.com>
2018-09-20 13:50:05 +0900

    firebase 추가

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux"
 import { createStackNavigator } from "react-navigation"
+import * as firebase from "firebase";
 
 import store from "./store"
 import InitialScreenContainer from "./containers/InitialScreenContainer";
 import LoginScreenContainer from "./containers/LoginScreenContainer";
+import NoteScreenContainer from "./containers/NoteScreenContainer";
+
+import { LoginScreen, NoteScreen, InitialScreen } from "./screenName";
+
+var config = {
+  apiKey: "AIzaSyD4y6p-amG4Ao_Mf1eE65-hDPHZMVxCU5Y",
+  authDomain: "interview-notepad.firebaseapp.com",
+  databaseURL: "https://interview-notepad.firebaseio.com",
+  projectId: "interview-notepad",
+  storageBucket: "interview-notepad.appspot.com",
+  messagingSenderId: "418843711159"
+};
+firebase.initializeApp(config);
 
 export default class App extends React.Component {
   render() {
@@ -23,5 +36,8 @@ const StackApp = createStackNavigator({
   },
   LoginScreen: {
     screen: LoginScreenContainer
+  },
+  NoteScreen: {
+    screen: NoteScreenContainer
   }
 })
