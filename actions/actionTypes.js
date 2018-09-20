458d9b15e6f21973e15f02a8972334d58fffd1e5
joylty94 <wowmen1757@gmail.com>
2018-09-20 13:50:42 +0900

    action 제거

--- actions/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const JOIN_PAGINATION = "loginScreen/JOIN_PAGINATION";
-export const LOGIN_PAGINATION = "loginScreen/LOGIN_PAGINATION";
--- actions/index.js
+++
@@ -1,9 +0,0 @@
-import { JOIN_PAGINATION, LOGIN_PAGINATION } from "./actionTypes";
-
-export const JoinPagination = () => ({
-  type: JOIN_PAGINATION,
-});
-
-export const LoginPagination = () => ({
-  type: LOGIN_PAGINATION,
-})
