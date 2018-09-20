83387bea76359bec133224eda7ab9058e473a950
joylty94 <wowmen1757@gmail.com>
2018-09-20 13:49:31 +0900

    NoteScreenContainer header 제거

--- containers/NoteScreenContainer.js
+++ containers/NoteScreenContainer.js
@@ -2,6 +2,10 @@ import React, { Component } from "react"
 import { View, StyleSheet, Text } from "react-native";
 
 export default class NoteScreenContainer extends Component{
+  static navigationOptions = ({navigation}) => {
+    let header = null
+    return { header }
+  }
   render(){
     return(
       <View>
