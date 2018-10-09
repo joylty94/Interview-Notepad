d5c0155b195dc243a1f99582dc33153f2cc2f439
joylty94 <wowmen1757@gmail.com>
2018-10-09 21:13:22 +0900

    react 추가, tabBarIcon 추가, createTabNavigator 변경

--- router.js
+++ router.js
@@ -1,42 +1,48 @@
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from "react";
+import { createStackNavigator, createBottomTabNavigator, createSwitchNavigator } from 'react-navigation';
+import { SimpleLineIcons } from "@expo/vector-icons";
 
 import NoteScreenContainer from './containers/NoteScreenContainer';
 import InitialScreenContainer from "./containers/InitialScreenContainer";
 import LoginScreenContainer from "./containers/LoginScreenContainer";
+import WritingNotesContainer from "./containers/WritingNotesContainer";
 
-let routeConfigs = {
-  Note: {
-    screen: NoteScreenContainer
-  },
-  // SharedNotes: {
-  //   screen: SharedNotesContainer,
-  // },
-  // Test: {
-  //   screen: TestContainer,
-  // }
-};
-let tabNavigatorConfig = {
-  animationEnabled: true,
-  swipeEnabled: true,
-  tabBarOptions: {
-    activeTintColor: 'blue',
-    labelStyle: {
-      fontSize: 13,
+export const NoteScreenScreen = createStackNavigator({
+  NoteScreenScreen: {
+    screen: NoteScreenContainer,
+  }
+})
+
+export const TabNavigator = createBottomTabNavigator(
+  {
+    StackNoteScreen: {
+      screen: NoteScreenScreen,
+      navigationOptions: ({
+        tabBarIcon: () => <SimpleLineIcons name="note" size={28} color="black" />
+      })
     },
-    style: {
-      backgroundColor: 'lightgray',
-      padding: -10
-    },
-    // showLabel: false
   },
+  {
+    animationEnabled: true,
+    swipeEnabled: true,
+    tabBarOptions: {
+      activeTintColor: 'black',
+      labelStyle: {
+        fontSize: 13,
+      },
+      style: {
+        backgroundColor: 'white',
+        padding: -10
+      },
+      showLabel: false
+    },
   // order: [Settings, Home, Cloud, Info],
-};
-
-export const TabNavigator = createBottomTabNavigator(routeConfigs, tabNavigatorConfig)
+  }
+)
 
 export const StackLogin = createStackNavigator({
   InitialScreen: {
-    screen: InitialScreenContainer,
+    screen: InitialScreenContainer
   },
   LoginScreen: {
     screen: LoginScreenContainer
@@ -44,18 +50,14 @@ export const StackLogin = createStackNavigator({
 })
 
 export const StackApp = createStackNavigator({
-  StackLoginScreen: {
-    screen: StackLogin,
-    navigationOptions: {
-      header: null
-    }
-  },
   TabScreen: {
     screen: TabNavigator,
-    navigationOptions: {
-      header: null
+    navigationOptions: () => {
+      let header = null;
+      return { header }
     }
+  },
+  WritingNotesScreen: {
+    screen: WritingNotesContainer,
   }
 })
-
-
