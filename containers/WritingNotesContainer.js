e7482eaad31d34d3075f93fb7585686825cb14b0
joylty94 <wowmen1757@gmail.com>
2018-10-09 21:12:11 +0900

    WritingNotesContainer 생성

---
+++ containers/WritingNotesContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet} from "react-native";
+
+export default class WritingNotesContainer extends Component{
+  render(){
+    return(
+      <View>
+        <Text>WritingNotes</Text>
+      </View>
+    )
+  }
+}
