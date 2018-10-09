479c9225e63e1f7ae98fd0b8f04b4df8c5d82500
joylty94 <wowmen1757@gmail.com>
2018-10-09 21:13:56 +0900

    navigationOptions 수정

--- containers/NoteScreenContainer.js
+++ containers/NoteScreenContainer.js
@@ -1,16 +1,39 @@
-import React, { Component } from "react"
-import { View, StyleSheet, Text } from "react-native";
+import React, { Component } from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { SimpleLineIcons } from "@expo/vector-icons";
+
+import { WritingNotesScreen } from "../screenName";
+import HeaderButton from "../components/HeaderButton";
 
 export default class NoteScreenContainer extends Component{
-  static navigationOptions = ({navigation}) => {
-    let header = null
-    return { header }
+  static navigationOptions = ({ navigation }) => {
+    let headerLeft = (
+      <TouchableOpacity style={styles.categoryButton}
+        onPress={() => navigation.navigate(WritingNotesScreen)}>
+        <Text style={styles.categoryText}>메모</Text>
+      </TouchableOpacity>
+    );
+    let headerRight = (
+      <HeaderButton/>
+    );
+    return { headerLeft, headerRight }
   }
   render(){
     return(
-      <View>
-        <Text>NoteScreenContainer</Text>
-      </View>
+      <Text>NOTE</Text>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  categoryButton : {
+    paddingHorizontal: 10,
+    paddingVertical : 15,
+    marginLeft: 8,
+  },
+  categoryText : {
+    fontSize : 20,
+    fontWeight: "bold",
+    textAlign : "center",
+  }
+})
