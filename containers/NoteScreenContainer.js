dc37fdb93beed2b9c5a70ca0d261cdec458004d3
joylty94 <wowmen1757@gmail.com>
2018-10-10 23:01:07 +0900

    Component 이름 수정

--- containers/NoteScreenContainer.js
+++ containers/NoteScreenContainer.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import { View, StyleSheet, Text, TouchableOpacity, Dimensions } from "react-native";
 
 import { WritingNotesScreen } from "../screenName";
-import HeaderButton from "../components/HeaderButton";
-import PlusButton from "../components/PlusButton";
+import HeaderButtonComponent from "../components/HeaderButtonComponent";
+import PlusButtonComponent from "../components/PlusButtonComponent";
 
 export default class NoteScreenContainer extends Component{
   static navigationOptions = ({ navigation }) => {
@@ -14,7 +14,7 @@ export default class NoteScreenContainer extends Component{
       </TouchableOpacity>
     );
     let headerRight = (
-      <HeaderButton/>
+      <HeaderButtonComponent/>
     );
     let headerStyle = ({
       backgroundColor: "rgb(145,167,255)"
@@ -26,7 +26,7 @@ export default class NoteScreenContainer extends Component{
     return(
       <View style={styles.container}>
         <Text>NOTE</Text>
-        <PlusButton {...rest}/>
+        <PlusButtonComponent {...rest}/>
       </View>
     )
   }
