21ff937c88d433e94821035cfb818db9d3987331
joylty94 <wowmen1757@gmail.com>
2018-10-10 23:01:34 +0900

    Component 이름 수정, backgroundColor 추가

--- containers/WritingNotesContainer.js
+++ containers/WritingNotesContainer.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, Platform } from "react-native";
 
+import WritingNotesButtonComponent from "../components/WritingNotesButtonComponent";
+import WritingNotesInputComponent from "../components/WritingNotesInputComponent";
+
 export default class WritingNotesContainer extends Component{
   render(){
+    const { ...rest } = this.props
     return(
       <View style={styles.container}>
-        <Text>WritingNotes</Text>
+        <WritingNotesButtonComponent {...rest}/>
+        <WritingNotesInputComponent {...rest} />
       </View>
     )
   }
@@ -14,6 +19,7 @@ export default class WritingNotesContainer extends Component{
 const styles = StyleSheet.create({
   container : {
     flex:1,
-    marginTop: Platform.OS === 'android' ? 20 : 0,
+    marginTop: Platform.OS === 'android' ? 23 : 0,
+    backgroundColor: "#fff"
   },
 })
