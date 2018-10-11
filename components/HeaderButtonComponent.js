47b62213871ab2f2c170ae4000da33ff9b2363af
joylty94 <wowmen1757@gmail.com>
2018-10-11 15:53:12 +0900

    Component name 수정

--- components/HeaderButtonComponent.js
+++ components/HeaderButtonComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { View, StyleSheet, TouchableOpacity, Text } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-export default class HeaderButton extends Component{
+export default class HeaderButtonComponent extends Component{
   render(){
     return(
       <View style={styles.constainer}>
