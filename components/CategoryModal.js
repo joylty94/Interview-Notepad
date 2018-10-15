0c630d8a70021051e720788beb7d880566cd50d1
joylty94 <wowmen1757@gmail.com>
2018-10-15 22:02:41 +0900

    카태고리 관리 추가

--- components/CategoryModal.js
+++ components/CategoryModal.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { View, Text, Modal, StyleSheet, ScrollView, FlatList, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons"
 
 export default class CategoryModal extends Component{
   render(){
-    const defaultCategory = [{ category: '메모장011111111111111111111111111111111111111111111111', number: 1 }, { category: '메모장', number: 23 }, { category: '메모장', number: 333 }, { category: '메모장0', number: 1 }, { category: '메모장', number: 23 }, { category: '메모장', number: 333 }, { category: '메모장0', number: 1 }, { category: '메모장', number: 23 }, { category: '메모장', number: 333 }, { category: '메모장0', number: 1 }, { category: '메모장', number: 23 }, { category: '메모장', number: 333 }, { category: '메모장0', number: 1 }, { category: '메모장', number: 23 }, { category: '메모장', number: 333 }, { category: '메모장0', number: 1 }, { category: '메모장', number: 23 }, { category: '메모장', number: 333 }, { category: '메모장0', number: 1 }, { category: '메모장', number: 23 }, { category: '메모장', number: 333 }, { category: '메모장0', number: 1 }, { category: '메모장', number: 23 }, { category: '메모장', number: 333 }]
+    const defaultCategory = [{ category: '메모장011111111111111111111111111111111111111111111111', number: 1 }, { category: '메모장', number: 23 }, { category: '메모장', number: 333 }, { category: '메모장0', number: 1 }, { category: '메모장', number: 23 }]
     return(
       <Modal
         animationType="slide"
@@ -14,6 +15,12 @@ export default class CategoryModal extends Component{
         }}>
         <View style={styles.modalContainer}>
           <View style={styles.modalView}>
+            <View style={styles.categoryHeader}>
+              <Text style={styles.categoryText}>카태고리</Text>
+              <TouchableOpacity>
+                <MaterialIcons name="settings" size={25} color="rgb(248,249,250)" />
+              </TouchableOpacity>
+            </View>
             <ScrollView>
               <FlatList
                 data={defaultCategory}
@@ -23,15 +30,14 @@ export default class CategoryModal extends Component{
                       // item={item}
                       // index={index}
                       style={styles.modalButton}>
-                        <Text style={styles.madalText}>{item.category}</Text>
-                        <Text style={styles.madalText}>{item.number}</Text>
+                      <Text style={styles.madalText}>{((item.category).length > 23) ? (item.category).substring(0, 20) + "..." : item.category}</Text>
+                      <Text style={styles.madalText}>{item.number}</Text>
                     </TouchableOpacity>
                   )
                 }}
                 keyExtractor={(item, index) => item.category + `${index}`}>
               </FlatList>
             </ScrollView>
-
           </View>
         </View>
       </Modal>
@@ -46,32 +52,40 @@ const styles = StyleSheet.create({
   },
   modalView: {
     width: "90%",
-    maxHeight: "50%",
-    marginTop: "4%",
+    maxHeight: "92%",
+    marginTop: 14,
     borderTopRightRadius: 5,
     borderTopLeftRadius: 5,
     borderBottomLeftRadius: 5,
     borderBottomRightRadius: 5,
-    borderWidth: 1,
-    borderColor: "black",
-    backgroundColor: "rgb(248,249,250)"
+    borderWidth: 2,
+    borderColor: "rgb(52,58,64)",
+    backgroundColor: "rgb(248,249,250)",
+  },
+  categoryHeader: {
+    backgroundColor: "rgb(145,167,255)",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 7,
+    paddingHorizontal: 15,
+  },
+  categoryText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "rgb(248,249,250)"
   },
   modalButton: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     paddingVertical: 7,
-    paddingHorizontal: 10,
+    paddingHorizontal: 15,
   },
   madalText: {
     textAlign: "center",
     fontSize: 18,
     fontWeight: "bold",
     color: "rgb(52,58,64)",
-  },
-  addCategoryButton: {
-    paddingVertical: 7,
-    paddingHorizontal: 10,
-    borderTopWidth: 1,
-  },
+  }
 })
