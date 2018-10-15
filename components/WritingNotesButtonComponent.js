15733421fb6d644600c4d20fd4951f5a741d9086
joylty94 <wowmen1757@gmail.com>
2018-10-15 22:33:32 +0900

    저장, 취소 버튼 ui 변경

--- components/WritingNotesButtonComponent.js
+++ components/WritingNotesButtonComponent.js
@@ -8,29 +8,35 @@ export default class WritingNotesButtonComponent extends Component{
       <View style={styles.container}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button}>
-            <FontAwesome name="bars" size={22} color="rgb(33,37,41)" />
+            <FontAwesome name="bars" size={23} color="rgb(33,37,41)" />
             <Text style={styles.buttonText}>카테고리</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button}>
-            <Feather name="check-square" size={22} color="rgb(33,37,41)" />
+            <Feather name="check-square" size={23} color="rgb(33,37,41)" />
             <Text style={styles.buttonText}>체 크</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <Feather name="image" size={22} color="rgb(33,37,41)" />
+          {/* <TouchableOpacity style={styles.button}>
+            <Feather name="image" size={23} color="rgb(33,37,41)" />
             <Text style={styles.buttonText}>이미지</Text>
-          </TouchableOpacity>
+          </TouchableOpacity> */}
           <TouchableOpacity style={styles.button}>
-            <FontAwesome name="tags" size={22} color="rgb(33,37,41)" />
+            <FontAwesome name="tags" size={23} color="rgb(33,37,41)" />
             <Text style={styles.buttonText}>태 그</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button}>
-            <FontAwesome name="share-alt" size={22} color="rgb(33,37,41)" />
+            <FontAwesome name="share-alt" size={23} color="rgb(33,37,41)" />
             <Text style={styles.buttonText}>공 유</Text>
           </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <FontAwesome name="save" size={23} color="rgb(33,37,41)" />
+            <Text style={styles.buttonText}>저 장</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}
+            onPress= {() => this.props.navigation.goBack()}>
+            <FontAwesome name="window-close" size={23} color="rgb(33,37,41)" />
+            <Text style={styles.buttonText}>취 소</Text>
+          </TouchableOpacity>
         </View>
-        <TouchableOpacity style={styles.saveContainer}>
-          <Text style={styles.saveText}>저 장</Text>
-        </TouchableOpacity>
       </View>
     )
   }
@@ -41,19 +47,19 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     paddingVertical: 5,
-    paddingHorizontal: 5,
+    paddingHorizontal: 8,
+    justifyContent: "center"
   },
   buttonContainer: {
     flexDirection: "row",
     borderColor: "rgb(134,142,150)",
     borderWidth: 1,
-    // paddingHorizontal: 5
   },
   button: {
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    width: 53,
+    width: 55,
     height: 45,
   },
   buttonText: {
