fe6a04d44e11a7968d299925206fde9904d17244
joylty94 <wowmen1757@gmail.com>
2018-10-18 23:04:52 +0900

    CategoryListHeaderComponent 추가

--- containers/CategoryListContainer.js
+++ containers/CategoryListContainer.js
@@ -4,13 +4,16 @@ import { View, StyleSheet } from "react-native";
 import CategoryListComponent from "../components/CategoryListComponent";
 import CategoryDeleteModalComponent from "../components/CategoryDeleteModalComponent";
 import CategoryUpdateModalComponent from "../components/CategoryUpdateModalComponent";
+import CategoryListHeaderComponent from "../components/CategoryListHeaderComponent";
 
 export default class CategoryListContainer extends Component{
   render(){
     return(
       <View style={{flex: 1}}>
+        <CategoryListHeaderComponent/>
         <CategoryListComponent/>
-
+        <CategoryDeleteModalComponent/>
+        <CategoryUpdateModalComponent/>
       </View>
     )
   }
