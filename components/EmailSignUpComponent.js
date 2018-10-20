099d286093295d28f6b49bc12b676b98049fc08a
joylty94 <wowmen1757@gmail.com>
2018-10-20 21:27:51 +0900

    EmailSignUpComponent ui 변경

--- components/EmailSignUpComponent.js
+++ components/EmailSignUpComponent.js
@@ -1,66 +1,153 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Text, TouchableOpacity, KeyboardAvoidingView } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity, TextInput, Platform, Alert } from "react-native";
+import { Button } from "native-base";
+// import { AuthLoading } from "../screenName";
 import * as firebase from "firebase";
-import { Container, Content, Header, Form, Input, Item, Button, Label } from "native-base";
 
 export default class EmailSignUpComponent extends Component {
+  constructor(props){
+    super(props)
+    this.state = ({
+      email : "",
+      password : "",
+      identifyPW : "",
+    })
+  }
+  signupUser = async (email, password, identifyPW) => {
+    try {
+      if (!email.includes("@")){
+        Alert.alert("이메일 오류!!!", "이메일 형식에 맞지 않습니다.")
+        return;
+      } else if (password.length < 8) {
+        Alert.alert("패스워드 오류!!!", "패스워드를 8글자 이상 입력해주세요.")
+        return;
+      } else if ( password !== identifyPW ) {
+        Alert.alert("패스워드 오류!!!", "패스워드가 같지 않습니다.")
+        return;
+      }
+      await firebase.auth().createUserWithEmailAndPassword(email, password)
+      await firebase.auth().signOut()
+      Alert.alert(
+        "회원가입",
+        "회원가입 완료!!!",
+        [
+          { text: 'OK', onPress: () => {
+            this.props.navigation.goBack()
+          }},
+        ],
+        { cancelable: false }
+      )
+    }
+    catch (error) {
+      console.log(e.toString())
+    }
+  }
   render(){
+    const { navigation } = this.props
     return(
-      <Container style={styles.container}>
-        <Form>
-          <Item floatingLabel>
-            <Label>Email</Label>
-            <Input
+      <View style={styles.container}>
+        <Text style={styles.titleText}>회원 가입</Text>
+        <View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.labelText}>아이디</Text>
+            <TextInput
+              style={styles.input}
+              placeholder=""
+              placeholderTextColor='rgb(134,142,150)'
+              underlineColorAndroid="rgba(255,255,255,0.2)"
+              keyboardType='email-address'
               autoCorrect={false}
               autoCapitalize="none"
-              onChangeText={(email) => this.setState({email})}
-              />
-          </Item>
-          <Item floatingLabel>
-            <Label>Password</Label>
-            <Input
+              returnKeyType='next'
+              onSubmitEditing={() => this.refs.password.focus()}
+              onChangeText={(email) => this.setState({ email })}
+            />
+            <Text style={styles.labelText}>패스워드</Text>
+            <TextInput
+              style={styles.input}
+              placeholder=""
+              placeholderTextColor='rgb(134,142,150)'
+              underlineColorAndroid="rgba(255,255,255,0.2)"
               secureTextEntry={true}
               autoCorrect={false}
               autoCapitalize="none"
+              returnKeyType='next'
+              ref={"password"}
+              onSubmitEditing={() => this.refs.identifyPW.focus()}
               onChangeText={(password) => this.setState({ password })}
-              />
-          </Item>
-          <Button
-            full
-            rounded
-            success
-            style={styles.buttonStyle}
-            onPress={() => this.signupUser(this.state.email, this.state.password)}>
-            <Text style={styles.buttonText}>SignUp</Text>
-          </Button>
-        </Form>
-        <Text>
-          회원가입
-        </Text>
-      </Container>
+            />
+            <Text style={styles.labelText}>패스워드 확인</Text>
+            <TextInput
+              style={styles.input}
+              placeholder=""
+              placeholderTextColor='rgb(134,142,150)'
+              underlineColorAndroid="rgba(255,255,255,0.2)"
+              secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize="none"
+              ref={"identifyPW"}
+              onChangeText={(identifyPW) => this.setState({ identifyPW })}
+            />
+          </View>
+            <Button
+              full
+              style={styles.button}
+              onPress={() => this.signupUser(this.state.email, this.state.password, this.state.identifyPW)}
+              >
+              <Text style={styles.buttonText}>SignUp</Text>
+            </Button>
+        </View>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex:1,
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: Platform.OS === "ios" ? 34 : 24,
+    paddingHorizontal: 20
   },
-  buttonStyle: {
-    width: "100%",
+  titleText: {
+    fontSize: 24,
+    color: "rgb(73,80,87)",
+    fontWeight: "bold",
+    marginBottom: 25
+  },
+  labelText: {
+    fontSize: 18,
+    color: "rgb(134,142,150)",
+    fontWeight: "300",
+    marginBottom: 8,
+  },
+  inputContainer: {
+    borderBottomWidth: 1,
+    borderColor: "rgb(206,212,218)",
+  },
+  input: {
+    height: 40,
+    width: 300,
+    backgroundColor: 'rgb(222,226,230)',
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    fontSize: 18,
+    color: "rgb(52,58,64)",
+    marginBottom: 20
+  },
+  button: {
+    height: 40,
     alignItems: "center",
     justifyContent: "center",
-    marginVertical: 10,
     padding: 5,
-    borderWidth: 1,
-    borderColor: "rgb(173,181,189)"
+    marginVertical: 20,
+    backgroundColor: "rgb(201,42,42)"
   },
   buttonText: {
-    marginLeft: 5,
     textAlign: 'center',
-    color: 'rgb(32, 53, 70)',
+    color: 'rgb(255,245,245)',
     fontWeight: 'bold',
     fontSize: 18
-  }
+  },
 })
