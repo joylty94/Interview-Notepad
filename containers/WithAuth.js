902ff25b9b8653c49e3a31db44de4a6d7a99f22a
joylty94 <wowmen1757@gmail.com>
2018-10-20 15:33:05 +0900

    WithAuth 생성

---
+++ containers/WithAuth.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { View, ActivityIndicator, StyleSheet, Platform } from "react-native";
+import * as firebase from 'firebase';
+import { App, Auth } from "../screenName";
+
+export default class WithAuth extends Component{
+  constructor(props) {
+    super(props);
+    this._bootstrapAsync();
+  }
+
+  _bootstrapAsync = async () => {
+    const { currentUser } = firebase.auth();
+    console.log("currentUser", currentUser)
+    if (currentUser) {
+      this.props.navigation.navigate(App)
+      };
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      console.log("user", user)
+      unsubscribe();
+      if (user) {
+        this.props.navigation.navigate(App)
+      } else {
+        this.props.navigation.navigate(Auth)
+      }
+    });
+  }
+
+  render(){
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    paddingTop: Platform.OS === 'ios' ? 34 : 24,
+  }
+})
