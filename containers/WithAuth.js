16dca7fa3d1aa48fdf46b365045821cf357e01cc
joylty94 <wowmen1757@gmail.com>
2018-10-21 20:09:05 +0900

    Loading 기능 구현

--- containers/WithAuth.js
+++ containers/WithAuth.js
@@ -1,32 +1,76 @@
 import React, { Component } from "react";
-import { View, ActivityIndicator, StyleSheet, Platform, StatusBar } from "react-native";
+import {
+  View, StyleSheet, Platform, Text, NativeModules, Image,
+  LayoutAnimation, ImageBackground } from "react-native";
 import * as firebase from 'firebase';
 import { App, Auth } from "../screenName";
+
+const { UIManager } = NativeModules;
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 
 export default class WithAuth extends Component{
   constructor(props) {
     super(props);
-    this._bootstrapAsync();
+    this.navigator();
+    this.state = ({
+      w: 260,
+      h: 230,
+    });
   }
-  _bootstrapAsync = async () => {
-    // const { currentUser } = firebase.auth();
-    // console.log("currentUser", currentUser)
-    // this.props.navigation.navigate(currentUser)
+
+  componentDidMount() {
+    requestAnimationFrame(() => {
+      timeId = setInterval(() => {
+        this._LayoutAnimation()
+      }, 650);
+    })
+  }
+
+  navigator = async () => {
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
       console.log("user", user)
       unsubscribe();
       if (user) {
-        this.props.navigation.navigate(App)
+        setTimeout(() => {
+          clearInterval(timeId)
+          this.props.navigation.navigate(App)
+        }, 2000)
       } else {
-        this.props.navigation.navigate(Auth)
+        setTimeout(() => {
+          clearInterval(timeId)
+          this.props.navigation.navigate(Auth)
+        }, 2000)
       }
     });
+  }
+
+  _LayoutAnimation = () => {
+    LayoutAnimation.spring();
+    console.log(this.state.w)
+    if (this.state.w === 260) {
+      this.setState({ w: this.state.w + 32, h: this.state.h + 26 })
+    } else {
+      this.setState({ w: this.state.w - 32, h: this.state.h - 26 })
+    }
   }
 
   render(){
     return (
       <View style={styles.container}>
-        <ActivityIndicator size="large" color="#0000ff" />
+        <ImageBackground
+          source={require("../images/note.png")}
+          style={styles.backgroundImage}
+        >
+          <Image
+            source={require('../images/book.png')}
+            style={[styles.imageContainer,
+              { width: this.state.w, height: this.state.h }
+            ]}>
+          </Image>
+            <Text style={[styles.loadingText]}>Loading...</Text>
+        </ImageBackground>
       </View>
     );
   }
@@ -34,7 +78,18 @@ export default class WithAuth extends Component{
 
 const styles = StyleSheet.create({
   container:{
-    flex:1,
     paddingTop: Platform.OS === 'ios' ? 34 : 24,
-  }
+    flex:1,
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loadingText: {
+    color: 'rgb(134,142,150)',
+    fontWeight: '500',
+    marginTop: 20,
+    fontSize: 24,
+  },
 })
