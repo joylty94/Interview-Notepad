6525ba8f3f9dbb9d1e0c58cdd25395d73880e006
joylty94 <wowmen1757@gmail.com>
2018-10-23 22:10:37 +0900

    카테고리, 태그, 저장 기능 구현

--- components/WritingNoteButtonComponent.js
+++ components/WritingNoteButtonComponent.js
@@ -3,12 +3,27 @@ import { View, StyleSheet, TouchableOpacity, Text, Platform } from "react-native
 import { FontAwesome, Feather } from "@expo/vector-icons";
 
 export default class WritingNoteButtonComponent extends Component{
+  handleModalButton = () => {
+    this.props.handleModal()
+  }
+  handleTagButton = () => {
+    this.props.handleTag()
+  }
+  handleShareButton = () => {
+    this.props.handleShare()
+  }
+  handleCreatingButton = (question, answer) => {
+    this.props.handleCreating(question, answer)
+    this.props.navigation.goBack()
+  }
   render(){
+    const { writingShare, writingTag, question, answer } = this.props
     return(
       <View style={styles.header}>
         <View style={styles.container}>
           <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button}
+              onPress={this.handleModalButton}>
               <FontAwesome name="bars" size={23} color="rgb(33,37,41)" />
               <Text style={styles.buttonText}>카테고리</Text>
             </TouchableOpacity>
@@ -16,19 +31,25 @@ export default class WritingNoteButtonComponent extends Component{
               <Feather name="check-square" size={23} color="rgb(33,37,41)" />
               <Text style={styles.buttonText}>체 크</Text>
             </TouchableOpacity>
-            {/* <TouchableOpacity style={styles.button}>
-              <Feather name="image" size={23} color="rgb(33,37,41)" />
-              <Text style={styles.buttonText}>이미지</Text>
-            </TouchableOpacity> */}
-            <TouchableOpacity style={styles.button}>
-              <FontAwesome name="tags" size={23} color="rgb(33,37,41)" />
-              <Text style={styles.buttonText}>태 그</Text>
+            <TouchableOpacity style={[styles.button,
+            { backgroundColor: writingTag ? "rgb(206,212,218)" : "rgb(248,249,250)"}]}
+              onPress={this.handleTagButton}>
+              <FontAwesome name="tags" size={23} color={writingTag ? "rgb(247,131,172)" : "rgb(33,37,41)"} />
+              <Text style={[styles.buttonText,
+                  { color: writingTag ? "rgb(248,249,250)" : "rgb(33,37,41)" }]}>
+                태 그
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-              <FontAwesome name="share-alt" size={23} color="rgb(33,37,41)" />
-              <Text style={styles.buttonText}>공 유</Text>
+            <TouchableOpacity style={[styles.button, { backgroundColor: writingShare ? "rgb(206,212,218)" : "rgb(248,249,250)" }]}
+              onPress={this.handleShareButton}>
+              <FontAwesome name="share-alt" size={23} color={writingShare ? "rgb(77,171,247)" : "rgb(33,37,41)"} />
+              <Text style={[styles.buttonText,
+                { color: writingShare ? "rgb(248,249,250)" : "rgb(33,37,41)" }]}>
+                공 유
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button}
+              onPress={() => this.handleCreatingButton(question, answer)}>
               <FontAwesome name="save" size={23} color="rgb(33,37,41)" />
               <Text style={styles.buttonText}>저 장</Text>
             </TouchableOpacity>
@@ -67,22 +88,13 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    width: 55,
+    width: 60,
     height: 45,
   },
   buttonText: {
     fontSize: 12,
     textAlign: "center",
+    fontWeight: "400",
     color: "rgb(33,37,41)"
   },
-  saveContainer: {
-    flex:1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  saveText: {
-    fontSize: 20,
-    textAlign: "center",
-    color: "rgb(230,119,0)"
-  }
 })
