701f0644e6ee0aece5c325774aed518b47fafed3
joylty94 <wowmen1757@gmail.com>
2018-10-23 22:06:38 +0900

    question, answer를 하위컴포넌트에서 수정할수 있는 함수생성

--- containers/WritingNoteScreenContainer.js
+++ containers/WritingNoteScreenContainer.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Platform } from "react-native";
+import { View, Text, StyleSheet, Platform, KeyboardAvoidingView } from "react-native";
+import { connect } from "react-redux";
 
+import CategoryListModalComponent from "../components/CategoryListModalComponent";
 import WritingNoteButtonComponent from "../components/WritingNoteButtonComponent";
 import WritingNoteInputComponent from "../components/WritingNoteInputComponent";
+import { fatchCategoryHandleModal, fatchHandleTag,
+  fatchHandleShare, fatchCreating } from "../thunk/writingNoteScreen";
 
-export default class WritingNoteScreenContainer extends Component{
+class WritingNoteScreenContainer extends Component{
+  constructor(props) {
+    super(props)
+    this.state = ({
+      question: "",
+      answer: ""
+    })
+  }
+
+  changeQuestion = (question) => {
+    this.setState({question})
+  }
+  changeAnswer = (answer) => {
+    this.setState({answer})
+  }
   render(){
     const { ...rest } = this.props
     return(
       <View style={styles.container}>
-        <WritingNoteButtonComponent {...rest}/>
-        <WritingNoteInputComponent {...rest} />
+        <WritingNoteButtonComponent {...rest} question={this.state.question}
+          answer={this.state.answer} />
+        <WritingNoteInputComponent {...rest} changeAnswer={this.changeAnswer}
+          changeQuestion={this.changeQuestion}/>
+        <CategoryListModalComponent {...rest} />
       </View>
     )
   }
@@ -22,3 +43,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
   },
 })
+
+export default connect (
+  state => ({
+    writingModal: state.writingNoteScreen.writingModal,
+    writingTag : state.writingNoteScreen.writingTag,
+    writingShare : state.writingNoteScreen.writingShare
+  }),
+  dispatch => ({
+    handleModal: () => {
+      dispatch(fatchCategoryHandleModal())
+    },
+    handleTag: () => {
+      dispatch(fatchHandleTag())
+    },
+    handleShare: () => {
+      dispatch(fatchHandleShare())
+    },
+    handleCreating: (question, answer) => {
+      dispatch(fatchCreating(question, answer))
+    }
+  })
+)(WritingNoteScreenContainer)
