96ecc025c5f8b0db7b171f183c0d7d55a53894c9
joylty94 <wowmen1757@gmail.com>
2018-10-24 21:43:24 +0900

    share 기능 구현

--- thunk/writingNoteScreen.js
+++ thunk/writingNoteScreen.js
@@ -1,6 +1,11 @@
 import * as firebase from "firebase";
 import { writingNoteScreenHandleCategory, writingNoteScreenHandleTag, writingNoteScreenHandleShare,
   writingNoteScreenHandleCreating } from "../actions/writingNoteScreen";
+import { noteScreenSuccess } from "../actions/noteScreen";
+const highestTimeoutId = setTimeout(() => ';');
+for (let i = 0; i < highestTimeoutId; i++) {
+  clearTimeout(i);
+}
 
 export const fatchCategoryHandleModal = () => (dispatch) => {
   dispatch(writingNoteScreenHandleCategory())
@@ -14,15 +19,48 @@ export const fatchHandleShare = () => (dispatch) => {
   dispatch(writingNoteScreenHandleShare())
 }
 
-export const fatchCreating = (question, answer) => async (dispatch, gestate) => {
-  const { uid } = firebase.auth().currentUser;
-  const stateItem = gestate();
-  const currentCategory = stateItem.noteScreen.currentCategory;
-  await firebase.database().ref(`users/${uid}/${currentCategory}/`).push({
-    question,
-    answer,
-    time: firebase.database.ServerValue.TIMESTAMP,
-    count: 1
-  })
-  dispatch(writingNoteScreenHandleCreating())
+export const fatchCreating = (question, answer, tag) => async (dispatch, gestate) => {
+  try{
+    const { uid } = firebase.auth().currentUser;
+    const stateItem = gestate();
+    const currentCategory = stateItem.noteScreen.currentCategory;
+    const countItem = stateItem.noteScreen.notesItem;
+    const count = countItem.length + 1;
+    const share = stateItem.writingNoteScreen.writingShare;
+    if (share) {
+      const notePush = firebase.database().ref(`users/${uid}/${currentCategory}/`).push({
+        question,
+        answer,
+        tag,
+        time: firebase.database.ServerValue.TIMESTAMP,
+        count
+      })
+      const sharePush = firebase.database().ref(`shared/`).push({
+        question,
+        answer,
+        tag,
+        time: firebase.database.ServerValue.TIMESTAMP,
+        uid,
+      })
+      await Promise.all([notePush, sharePush])
+    } else {
+      await firebase.database().ref(`users/${uid}/${currentCategory}/`).push({
+        question,
+        answer,
+        tag,
+        time: firebase.database.ServerValue.TIMESTAMP,
+        count
+      })
+    }
+    dispatch(writingNoteScreenHandleCreating())
+    const snapshot = await firebase.database().ref(`users/${uid}/${currentCategory}`).once("value");
+    const notesObject = snapshot.val() || [];
+    const notesItem = Object.entries(notesObject).map(([category, article]) => ({
+      ...article,
+    }));
+    const sortNotesItem = notesItem.reverse()
+    dispatch(noteScreenSuccess(currentCategory, sortNotesItem))
+  } catch(e) {
+    console.log(e)
+  }
 }
