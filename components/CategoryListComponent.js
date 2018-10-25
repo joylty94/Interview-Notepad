e0a7430a02a8c7e4c5d5f49ddbf7766306b679bd
joylty94 <wowmen1757@gmail.com>
2018-10-25 16:12:53 +0900

    currentCategory, CategoryItem으로 List 생성

--- components/CategoryListComponent.js
+++ components/CategoryListComponent.js
@@ -4,17 +4,18 @@ import { EvilIcons } from "@expo/vector-icons";
 
 export default class CategoryListComponent extends Component{
   render(){
-    const defaultCategory = [{ category: '메모장011111111111111111111111111111111111111111111111', number: 1 }, { category: '메모장', number: 23 }, { category: '메모장', number: 333 }, { category: '메모장0', number: 1 }, { category: '메모장', number: 23 }]
+    const { currentCategory, categoryItem } = this.props;
     return(
       <View style={{ flex: 1, paddingHorizontal: 15,}}>
         <View style={styles.listContainer}>
           <FlatList
-            data={defaultCategory}
+            data={categoryItem}
             renderItem={({ item, index }) => {
               return (
                 <View style={styles.listItem}>
-                  <Text style={styles.text}>
-                    {((item.category).length > 23) ? (item.category).substring(0, 20) + "..." : item.category}
+                  <Text style={[styles.text,
+                    { color: currentCategory === item.categoryName ? "rgb(245,159,0)" : "rgb(52,58,64)"}]}>
+                    {((item.categoryName).length > 23) ? (item.categoryName).substring(0, 20) + "..." : item.categoryName}
                   </Text>
                 </View>
               )
@@ -43,7 +44,6 @@ const styles = StyleSheet.create({
   text: {
     fontWeight: "400",
     fontSize: 18,
-    color: "rgb(52,58,64)",
     marginLeft:2
   },
   addContainer: {
