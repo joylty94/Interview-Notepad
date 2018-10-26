d6cca287a135a51c5af75b82f020ac94de356111
joylty94 <wowmen1757@gmail.com>
2018-10-26 20:34:21 +0900

    componentDidMount시 categoryItem 읽어오기 기능

--- thunk/noteScreen.js
+++ thunk/noteScreen.js
@@ -8,44 +8,41 @@ export const fetchNoteScreen = () => async (dispatch) => {
   const snapshot = await firebase.database().ref(`/users/${uid}/currentCategory`).once("value");
   const current = snapshot.val() || [];
   if (current[0] === undefined){
-    await firebase.database().ref(`users/${uid}/categorys/`).push({
+    initCategory = firebase.database().ref(`users/${uid}/categorys/`).push({
       categoryName: "메모장",
-      count: 0,
+      count: 1,
       noteCount: 0,
     })
-    await firebase.database().ref(`users/${uid}/currentCategory/`).set("메모장")
-    dispatch(noteScreenSuccess("메모장", []))
+    initCurrent = firebase.database().ref(`users/${uid}/currentCategory/`).set("메모장")
+    await Promise.all([initCategory, initCurrent])
+    const snapshot = await firebase.database().ref(`users/${uid}/categorys`).once("value");
+    const categoryObject = snapshot.val() || [];
+    const categoryItem = Object.entries(categoryObject).map(([id, article]) => ({
+      id,
+      ...article,
+    }));
+    dispatch(noteScreenSuccess("메모장", [], categoryItem))
   }else {
     const snapshot = await firebase.database().ref(`users/${uid}/${current}`).once("value");
     const notesObject = snapshot.val() || [];
-    const notesItem = Object.entries(notesObject).map(([category, article]) => ({
+    const notesItem = Object.entries(notesObject).map(([id, article]) => ({
+      id,
       ...article,
     }));
     const sortNotesItem = notesItem.reverse()
-    dispatch(noteScreenSuccess(current, sortNotesItem))
+    const snapshot2 = await firebase.database().ref(`users/${uid}/categorys`).once("value");
+    const categoryObject = snapshot2.val() || [];
+    const categoryItem = Object.entries(categoryObject).map(([id, article]) => ({
+      id,
+      ...article,
+    }));
+    dispatch(noteScreenSuccess(current, sortNotesItem, categoryItem))
   }
   } catch(e) {
     console.log(e);
   }
 }
 
-export const fetchcategoryOnModal = () => async (dispatch, gestate) => {
-  try{
-    const { uid } = firebase.auth().currentUser;
-    const stateItem = gestate();
-    const modal = stateItem.noteScreen.noteModal
-    if(!modal){
-      const snapshot = await firebase.database().ref(`users/${uid}/categorys`).once("value")
-      const categoryObject = snapshot.val() || [];
-      const categoryItem = Object.entries(categoryObject).map(([category, article]) => ({
-        ...article
-      }))
-      dispatch(noteScreenOnHandleModal(categoryItem))
-    } else {
-      const categoryItem = stateItem.noteScreen.categoryItem
-      dispatch(noteScreenOnHandleModal(categoryItem))
-    }
-  } catch(e) {
-    console.log(e)
-  }
+export const fetchcategoryOnModal = () => async (dispatch) => {
+  dispatch(noteScreenOnHandleModal())
 }
