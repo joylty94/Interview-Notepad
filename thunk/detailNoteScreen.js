197cadfc0f89bed07d733dd90136da18c1a422b9
joylty94 <wowmen1757@gmail.com>
2018-10-30 18:54:25 +0900

    notes 경로 추가

--- thunk/detailNoteScreen.js
+++ thunk/detailNoteScreen.js
@@ -7,7 +7,7 @@ export const fetchDetailNoteScreen = (item) => async(dispatch, getstate) => {
   const stateItem = getstate();
   const currentCategory = stateItem.noteScreen.currentCategory;
   const { uid } = firebase.auth().currentUser;
-  const snapshot = await firebase.database().ref(`/users/${uid}/${currentCategory}/${item.id}`).once("value")
+  const snapshot = await firebase.database().ref(`/users/${uid}/notes/${currentCategory}/${item.id}`).once("value")
   const detailCategoryObject = snapshot.val() || [];
   const detailCategory = Object.assign({}, detailCategoryObject, {id: item.id})
   dispatch(detailNoteScreenSuccess(detailCategory))
