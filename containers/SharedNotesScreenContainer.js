674acbc6725fb301ca15bb39e4d46ebff9812aaa
joylty94 <wowmen1757@gmail.com>
2018-10-31 19:38:55 +0900

    SharedNotesScreenContainer 생성

---
+++ containers/SharedNotesScreenContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { View, StyleSheet} from "react-native";
+
+import SharedNotesHeaderComponent from "../components/SharedNotesHeaderComponent";
+
+export default class SharedNotesScreenContainer extends Component{
+  render(){
+    return(
+      <View style={{flex:1}}>
+      </View>
+    )
+  }
+}
