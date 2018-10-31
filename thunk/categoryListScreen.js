b11e1678c4e7a2d3305a88e5e72b0ca4b749f887
joylty94 <wowmen1757@gmail.com>
2018-10-31 09:56:49 +0900

    currentCategory일 때, 수정, 삭제 기능 구현

--- thunk/categoryListScreen.js
+++ thunk/categoryListScreen.js
@@ -2,6 +2,7 @@ import * as firebase from "firebase";
 import { categoryListScreenLoading, categoryListScreenSuccess, categoryListScreenAddCategory,
   categoryListScreenUpdateCategory, categoryListScreenDeleteCategory } from "../actions/categoryListScreen";
 import { fetchNoteScreen } from "./noteScreen"
+import { InputGroup } from "native-base";
 
 export const fetchCategoryListScreen = () => async (dispatch) => {
   try{
@@ -46,15 +47,27 @@ export const fetchAddCategory = (text) => async (dispatch) => {
   }
 }
 
-export const fetchUpdateCategory = (text, category) => async(dispatch) => {
+export const fetchUpdateCategory = (text, category) => async(dispatch, getstate) => {
   try{
+    const stateItem = getstate();
+    const currentCategory = stateItem.noteScreen.currentCategory;
     if (text.length >= 1) {
       const { uid } = firebase.auth().currentUser;
-      await firebase.database().ref(`/users/${uid}/categorys/${category.id}`).update({
-        categoryName: text,
-        count: category.count,
-        noteCount: category.noteCount
-      })
+      if(currentCategory===category.categoryName){
+        const updateCurrentCategory = firebase.database().ref(`users/${uid}/currentCategory/`).set(`${text}`)
+        const updateCategory = firebase.database().ref(`/users/${uid}/categorys/${category.id}`).update({
+          categoryName: text,
+          count: category.count,
+          noteCount: category.noteCount
+        })
+        await Promise.all([updateCurrentCategory, updateCategory])
+      } else {
+        await firebase.database().ref(`/users/${uid}/categorys/${category.id}`).update({
+          categoryName: text,
+          count: category.count,
+          noteCount: category.noteCount
+        })
+      }
     }
     dispatch(categoryListScreenUpdateCategory())
     dispatch(fetchCategoryListScreen())
@@ -63,10 +76,26 @@ export const fetchUpdateCategory = (text, category) => async(dispatch) => {
     console.log(e)
   }
 }
-export const fetchDeleteCategory = (category) => async(dispatch) => {
+export const fetchDeleteCategory = (category) => async(dispatch, getstate) => {
   try{
     const { uid } = firebase.auth().currentUser;
-    await firebase.database().ref(`/users/${uid}/categorys/${category.id}`).remove()
+    const stateItem = getstate();
+    const currentCategory = stateItem.noteScreen.currentCategory;
+    const categoryItem = stateItem.noteScreen.categoryItem
+    console.log("요거", categoryItem[1]["categoryName"])
+    if (currentCategory===category.categoryName){
+      if (categoryItem[0]["categoryName"] === currentCategory) {
+        const deleteCategory = firebase.database().ref(`/users/${uid}/categorys/${category.id}`).remove()
+        const updateCurrentCategory = firebase.database().ref(`users/${uid}/currentCategory/`).set(`${categoryItem[1]["categoryName"]}`)
+        await Promise.all([deleteCategory, updateCurrentCategory])
+      } else {
+        const deleteCategory = firebase.database().ref(`/users/${uid}/categorys/${category.id}`).remove()
+        const updateCurrentCategory = firebase.database().ref(`users/${uid}/currentCategory/`).set(`${categoryItem[0]["categoryName"]}`)
+        await Promise.all([deleteCategory, updateCurrentCategory])
+      }
+    } else {
+      await firebase.database().ref(`/users/${uid}/categorys/${category.id}`).remove()
+    }
     dispatch(categoryListScreenDeleteCategory())
     dispatch(fetchCategoryListScreen())
     dispatch(fetchNoteScreen())
