5ab9fd8b091200e33e17338d1d26ab5cdc980798
joylty94 <wowmen1757@gmail.com>
2018-10-31 19:42:29 +0900

    SharedNotesScreenContainer 코드제거

--- containers/SharedNotesScreenContainer.js
+++ containers/SharedNotesScreenContainer.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { View, StyleSheet} from "react-native";
 
-import SharedNotesHeaderComponent from "../components/SharedNotesHeaderComponent";
-
 export default class SharedNotesScreenContainer extends Component{
   render(){
     return(
