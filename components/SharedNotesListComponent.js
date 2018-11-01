51295e30a75aba3c1f16b9b3ce2daea750df4334
joylty94 <wowmen1757@gmail.com>
2018-11-01 21:54:37 +0900

    초기화면 ui 변경

--- components/SharedNotesListComponent.js
+++ components/SharedNotesListComponent.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import { View, StyleSheet, ScrollView, FlatList } from "react-native";
+import { View, StyleSheet, ScrollView, FlatList, Text } from "react-native";
 
 import SharedNotesListAnimatedComponent from "./SharedNotesListAnimatedComponent"
 
 export default class SharedNotesListComponent extends Component{
   render(){
     const { results, ...rest } = this.props;
+    if(results.length === 0){
+      return(
+        <View style={styles.initContainer}>
+          <Text style={styles.initText}>면접 질문을 입력하세요.</Text>
+        </View>
+      )
+    }
     return(
       <ScrollView style={styles.container}>
         <FlatList
@@ -29,4 +36,14 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
     marginBottom: 10,
   },
+  initContainer: {
+    flex: 1,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  initText: {
+    fontSize: 20,
+    fontWeight:"400",
+    color:"rgb(73,80,87)",
+  }
 })
