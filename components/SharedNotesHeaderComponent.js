57c5b653bbe56283f3e464cea4af822c8d2a0d7f
joylty94 <wowmen1757@gmail.com>
2018-11-02 14:01:56 +0900

    sharedNotesScreen Header ui 변경

--- components/SharedNotesHeaderComponent.js
+++ components/SharedNotesHeaderComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Platform } from "react-native";
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from "react-native";
+import { Foundation } from "@expo/vector-icons";
 import SearchBar from "react-native-searchbar";
 
 export default class SharedNotesHeaderComponent extends Component {
@@ -15,7 +16,22 @@ export default class SharedNotesHeaderComponent extends Component {
             hideBack={true}
             showOnLoad={true}
             focusOnLayout={false}
+            backgroundColor="rgb(145,167,255)"
+            textColor="rgb(248,249,250)"
+            iconColor="rgb(248,249,250)"
+            placeholderTextColor="rgb(241,243,245)"
+            heightAdjust={-10}
           />
+        </View>
+        <View style={styles.tabContainer}>
+          <TouchableOpacity
+            style={styles.tabButton}
+            onPress={() => this.props.navigation.goBack()}>
+            <Foundation name="page-edit" size={25} color="rgb(145,167,255)" />
+          </TouchableOpacity>
+          <View style={styles.tabActiveButton}>
+            <Foundation name="page-search" size={25} color="rgb(145,167,255)" />
+          </View>
         </View>
       </View>
     )
@@ -25,10 +41,30 @@ export default class SharedNotesHeaderComponent extends Component {
 const styles = StyleSheet.create({
   container: {
     paddingTop: Platform.OS === 'ios' ? 34 : 24,
-    height: 85,
+    backgroundColor: "rgb(145,167,255)"
   },
   header: {
+    justifyContent: "center",
+    height: 50,
+  },
+  tabContainer: {
+    flexDirection: "row",
+    height: 50,
+    backgroundColor: "rgb(248,249,250)"
+  },
+  tabActiveButton: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+    borderBottomWidth: 3,
+    borderColor: "rgb(145,167,255)"
+  },
+  tabButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "rgb(134,142,150)",
+    paddingBottom: 2
   }
 })
