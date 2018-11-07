fcfcd2a30353d79fa4321939bb0237786779dcbf
joylty94 <wowmen1757@gmail.com>
2018-11-07 13:56:19 +0900

    EmailSignInComponent ui 변경

--- components/EmailSignInComponent.js
+++ components/EmailSignInComponent.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { View, StyleSheet, Text, TouchableOpacity, TextInput, Alert } from "react-native";
 import * as firebase from "firebase";
 import { Button } from "native-base";
-import { App, SignUpScreen, AuthLoading } from "../screenName";
+import { App, SignUpScreen } from "../screenName";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default class EmailSignInComponent extends Component {
   constructor(props) {
@@ -16,8 +17,28 @@ export default class EmailSignInComponent extends Component {
 
   signInUser = (email, password) => {
     try{
+      if(email.length === 0) {
+        Alert.alert(
+          "",
+          "이메일을 입력해주세요.",
+          [
+            { text: "OK" }
+          ],
+          { cancelable: false }
+        )
+        return ;
+      } else if(password.length === 0) {
+        Alert.alert(
+          "",
+          "패스워드를 입력해주세요.",
+          [
+            { text: "OK" }
+          ],
+          { cancelable: false }
+        )
+        return;
+      }
       firebase.auth().signInWithEmailAndPassword(email, password).then(user => {
-        console.log("1",user)
         this.props.navigation.navigate(App)
       }).catch(e => {
         const errorCode = e.code;
@@ -67,41 +88,49 @@ export default class EmailSignInComponent extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          placeholder="email"
-          placeholderTextColor='rgb(134,142,150)'
-          underlineColorAndroid="rgba(255,255,255,0.2)"
-          autoCorrect={false}
-          autoCapitalize="none"
-          returnKeyType='next'
-          keyboardType='email-address'
-          onSubmitEditing={() => this.refs.txtPassword.focus()}
-          onChangeText={(email) => this.setState({ email })}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="password"
-          placeholderTextColor='rgb(134,142,150)'
-          underlineColorAndroid="rgba(255,255,255,0.2)"
-          secureTextEntry={true}
-          autoCorrect={false}
-          autoCapitalize="none"
-          ref={"txtPassword"}
-          onChangeText={(password) => this.setState({ password })}
-        />
+        <View style={styles.inputContainer}>
+          <View style={styles.inputIcon}><FontAwesome name="user" size={25} color="rgb(248,249,250)" /></View>
+          <TextInput
+            style={styles.input}
+            placeholder="email"
+            placeholderTextColor='rgb(173,181,189)'
+            underlineColorAndroid='rgb(248,249,250)'
+            autoCorrect={false}
+            autoCapitalize="none"
+            returnKeyType='next'
+            keyboardType='email-address'
+            onSubmitEditing={() => this.refs.txtPassword.focus()}
+            onChangeText={(email) => this.setState({ email })}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <View style={styles.inputIcon}><FontAwesome name="lock" size={25} color="rgb(248,249,250)" /></View>
+          <TextInput
+            style={styles.input}
+            placeholder="password"
+            placeholderTextColor='rgb(173,181,189)'
+            underlineColorAndroid='rgb(248,249,250)'
+            secureTextEntry={true}
+            autoCorrect={false}
+            autoCapitalize="none"
+            ref={"txtPassword"}
+            onChangeText={(password) => this.setState({ password })}
+          />
+        </View>
         <View style={styles.buttonContainer}>
           <Button
             full
             style={styles.button}
             onPress={() => this.signInUser(this.state.email, this.state.password)}>
-            <Text style={styles.buttonText}>로그인</Text>
+            <Text style={styles.buttonText}>LOGIN</Text>
           </Button>
         </View>
-        <TouchableOpacity style={styles.signUpButton}
-          onPress={() => this.props.navigation.navigate(SignUpScreen)}>
-          <Text style={styles.signUpText}>회원가입</Text>
-        </TouchableOpacity>
+        <View style={styles.signUpContainer}>
+          <TouchableOpacity style={styles.signUpButton}
+            onPress={() => this.props.navigation.navigate(SignUpScreen)}>
+            <Text style={styles.signUpText}>회원가입</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
@@ -113,40 +142,68 @@ const styles = StyleSheet.create({
     alignItems:"center",
     justifyContent: "center",
   },
+  inputContainer:{
+    flexDirection:"row",
+    alignItems: "center",
+    height: 50,
+    width: 300,
+    backgroundColor: 'rgb(248,249,250)',
+    borderWidth:1,
+    borderColor: "rgb(255,135,135)",
+    borderRadius: 30,
+    marginBottom:10,
+  },
   input: {
     height: 40,
-    width: 300,
-    backgroundColor: 'rgb(222,226,230)',
-    marginBottom: 15,
+    width: 260,
+    fontFamily: "GodoB",
     paddingHorizontal: 10,
     fontSize: 18,
     color: "rgb(52,58,64)",
   },
+  inputIcon: {
+    height:50,
+    width:50,
+    justifyContent:"center",
+    alignItems: "center",
+    backgroundColor:"rgb(255,135,135)",
+    borderRadius:30
+  },
   buttonContainer: {
     width: 300,
     borderBottomWidth: 1,
-    borderColor: "rgb(206,212,218)"
+    borderColor: "rgb(255,201,201)"
   },
   button: {
-    height: 40,
+    height: 50,
     alignItems: "center",
     justifyContent: "center",
     padding: 5,
-    marginBottom: 15,
-    backgroundColor: "rgb(201,42,42)"
+    backgroundColor: "rgb(248,249,250)",
+    borderWidth: 1,
+    borderColor: "rgb(255,107,107)",
+    borderRadius: 30,
+    marginBottom: 10,
   },
   buttonText: {
     textAlign: 'center',
-    color: 'rgb(255,245,245)',
+    color: 'rgb(255,135,135)',
     fontWeight: 'bold',
     fontSize: 18
   },
+  signUpContainer:{
+    width: 300,
+    alignItems: "flex-end"
+  },
   signUpButton: {
     padding: 5,
+    width: 70,
   },
   signUpText: {
+    fontFamily: "BMYEONSUNG",
     textAlign: "right",
     fontSize: 16,
-    color: "rgb(134,142,150)"
+    fontWeight: "400",
+    color: "rgb(52,58,64)"
   }
 })
